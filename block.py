c0aa023a26561eedefcdde3e7a42afe58867169c
Simmo Saan <simmo.saan@gmail.com>
2016-01-21 13:21:41 +0200

    Add end_time property

--- block.py
+++ block.py
@@ -11,6 +11,9 @@ class Row:
         self.total_time = self.row_time + self.cleanup_time
         self.start_time = block.start_time + i * self.total_time
 
+    @property
+    def end_time(self):
+        return self.start_time + self.total_time
 
 class Block:
     def __init__(self, row):
@@ -21,9 +24,17 @@ class Block:
         self.row_time = datetime.timedelta(minutes=int(row['row_time']))
         self.cleanup_time = datetime.timedelta(minutes=int(row['cleanup_time']))
 
+        self.rows = []
+        self.total_time = datetime.timedelta()
+
+    @property
+    def end_time(self):
+        return self.start_time + self.total_time
+
     def create_rows(self, teamcnt):
         rowcnt = math.ceil(teamcnt / self.columns)
         self.rows = [Row(self, i) for i in range(rowcnt)]
+        self.total_time = rowcnt * (self.row_time + self.cleanup_time)
 
     @staticmethod
     def load(filename):
